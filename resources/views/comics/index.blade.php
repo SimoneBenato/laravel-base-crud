2bd4d01a5a71b5b8b7f2342cb538fe69c1985cf3
SimoneBenato <sb270298@gmail.com>
2021-11-04 17:59:46 +0100

    Stampaggio dei dati sulla view index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,5 +1,13 @@
 @foreach ($comics as $comic)
 <div>
-    {{$comic->title}}
+    <div>
+        {{$comic->thumb}}
+    </div>
+    <h3>{{$comic->title}}</h3>
+    <p>{{$comic->description}}</p>
+    <p>{{$comic->price}}</p>
+    <p>{{$comic->series}}</p>
+    <p>{{$comic->sale_date}}</p>
+    <p>{{$comic->type}}</p>
 </div>    
 @endforeach
