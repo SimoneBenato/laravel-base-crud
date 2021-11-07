67dd1bead4bd15da7c4825d93aba20ded1b52d43
SimoneBenato <sb270298@gmail.com>
2021-11-07 10:42:56 +0100

    Sistemazione dati tramite tabella

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,13 +1,48 @@
-@foreach ($comics as $comic)
-<div>
-    <div>
-        <img src="{{$comic->thumb}}" alt="">
-    </div>
-    <h3>{{$comic->title}}</h3>
-    <p>{{$comic->description}}</p>
-    <p>{{$comic->price}}</p>
-    <p>{{$comic->series}}</p>
-    <p>{{$comic->sale_date}}</p>
-    <p>{{$comic->type}}</p>
-</div>    
-@endforeach
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Comic Index</title>
+</head>
+<body>
+    @yield('navbar')
+
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">cover</th>
+            <th scope="col">title</th>
+            <th scope="col">description</th>
+            <th scope="col">price</th>
+            <th scope="col">series</th>
+            <th scope="col">sale_date</th>
+            <th scope="col">type</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($comics as $comic)
+                <tr>
+                    <th scope="row"><img src="{{$comic->thumb}}" alt=""></th>
+                    <td>{{$comic->title}}</td>
+                    <td>{{$comic->description}}</td>
+                    <td>{{$comic->price}}</td>
+                    <td>{{$comic->series}}</td>
+                    <td>{{$comic->sale_date}}</td>
+                    <td>{{$comic->type}}</td>
+                    <td>
+                        <a class="btn btn-warning" href="#">Dettagli</a>
+                        <br>
+                        <a class="btn btn-success" href="#">Modifica</a>
+                        <br>
+                        <a class="btn btn-danger" href="#">Elimina</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+</html>
